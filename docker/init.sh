feb966c37569a3f0961879a5a77fa4afd566dd00
James Levell <jimmy.levell@outlook.com>
2020-10-12 11:04:22 +0200

    fix(docker): fixed typo in docker image

--- docker/init.sh
+++ docker/init.sh
@@ -10,7 +10,7 @@ npx sequelize db:create
 npx sequelize db:migrate
 
 # change permission on the database streetmix to a new user streetmix
-psql -v ON_ERROR_STOP=1 -d streetmi <<- EOSQL
+psql -v ON_ERROR_STOP=1 -d streetmix <<- EOSQL
     CREATE USER streetmix;
     GRANT ALL PRIVILEGES ON DATABASE streetmix TO streetmix;
     GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO streetmix;
